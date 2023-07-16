9ce8d4c7d2912c3ed15bad29b091825b91bd680f
Paul SJ <sjpaul19@gmail.com>
2023-07-16 15:41:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * This is my first C program on Linux
- *
- * return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
