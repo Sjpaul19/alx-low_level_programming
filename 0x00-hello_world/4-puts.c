a83a798445cdae0775eb2e7cce6761bdc0502002
Paul SJ <sjpaul19@gmail.com>
2023-07-16 15:39:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  * main - entry point
  * This is my first C program on Linux
+ * return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
