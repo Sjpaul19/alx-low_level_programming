c9dc0e786690af271c1be69fc2ecea0646c33fde
Paul SJ <sjpaul19@gmail.com>
2023-07-16 15:36:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * This is my first C program on Linux
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
