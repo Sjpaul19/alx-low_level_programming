e787dc57f3fe344a03b63769fcdd80bcd0476e2e
Paul SJ <sjpaul19@gmail.com>
2023-08-06 11:03:13 -0700

    I learnt argc and argv[]

---
+++ 0x09-static_libraries/args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main: the main program
+ * return: always 0
+ * argc: count of arguments
+ * argv[]: vector argument
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	printf("argc = %d\n", argc);
+	printf("Let's see what is in argv[]\n");
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	return 0;
+}
